c097d3a79fe97ab3d770836d274a80800f2c2152
Daniele Tricoli <eriol@mornie.org>
2019-05-13 02:27:25 +0200

    Use clap for the CLI
    - Add "serve" subcommand to start the UDP server.
    - Print "help" if no subcommand is supplied.

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,58 @@ mod network;
 use std::sync::{Arc, RwLock};
 use std::thread;
 
+use clap::{crate_authors, crate_version, App, AppSettings, Arg, SubCommand};
 use log::{error, info};
 use pretty_env_logger;
 
 use commands::Command;
 use network::SimpleUDPServer;
 
+const ABOUT: &str = "
+Display custom icons on system tray.
+Supported format for icons are SVG and PNG.";
+const DEFAULT_HOST: &str = "127.0.0.1";
+const DEFAULT_PORT: &str = "1738";
+
 fn main() {
     pretty_env_logger::init_timed();
 
-    let command = Arc::new(RwLock::new(Command::from("black")));
+    let matches = App::new("quoll")
+        .about(ABOUT)
+        .author(crate_authors!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .takes_value(true)
+                .value_name("PORT"),
+        )
+        .subcommand(
+            SubCommand::with_name("serve")
+                .about("Starts UDP server for incoming commands"),
+        )
+        .get_matches();
 
-    info!("Starting UDP server...");
-    let mut server =
-        SimpleUDPServer::new("127.0.0.1:1234", Arc::clone(&command));
-    thread::spawn(move || {
-        if let Err(e) = server.serve_forever() {
-            error!("{}", e);
-        };
-    });
+    let port = matches.value_of("port").unwrap_or(DEFAULT_PORT);
 
-    info!("Starting GUI...");
-    gui::start(command);
-    info!("Exiting...");
+    if let Some(_) = matches.subcommand_matches("serve") {
+        let command = Arc::new(RwLock::new(Command::from("black")));
+
+        info!("Starting UDP server on port {}...", port);
+        let mut server = SimpleUDPServer::new(
+            format!("{}:{}", DEFAULT_HOST, port),
+            Arc::clone(&command),
+        );
+        thread::spawn(move || {
+            if let Err(e) = server.serve_forever() {
+                error!("{}", e);
+            };
+        });
+
+        info!("Starting GUI...");
+        gui::start(command);
+        info!("Exiting...");
+    }
 }
